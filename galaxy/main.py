328d70c59db048d5dcc94103bbe1a2a054266985
Josh Abbott <39091681+Aoshua@users.noreply.github.com>
2021-08-27 15:06:22 -0600

    forward motion

--- galaxy/main.py
+++ galaxy/main.py
@@ -3,6 +3,7 @@ from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty
 from kivy.graphics.context_instructions import Color
 from kivy.graphics.vertex_instructions import Line
+from kivy.properties import Clock
 
 # kivy.graphics.vertex_instructions.Line(VertexInstruction)
 
@@ -18,10 +19,14 @@ class MainWidget(Widget):
     H_LINE_SPACING = .1 #% of screen height
     horizontalLines = []
 
+    SPEED = 2
+    currentYOffset = 0
+
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
         self.init_vertical_lines()
         self.init_horizontal_lines()
+        Clock.schedule_interval(self.update, 1.0 / 60.0) # 60fps
 
     # Called when the widget is attached to the parent
     def on_parent(self, widget, parent): 
@@ -29,8 +34,9 @@ class MainWidget(Widget):
 
     # Called on initally sizing the window, and when window size changes
     def on_size(self, *args): 
-        self.update_vertical_lines()
-        self.update_horizontal_lines()
+        # self.update_vertical_lines()
+        # self.update_horizontal_lines()
+        pass
 
     # All class properties have this form of on change function:
     def on_xPerspective(self, widget, value):
@@ -73,7 +79,7 @@ class MainWidget(Widget):
         xMax = centerLineX - offset*spacing
         ySpacing = self.H_LINE_SPACING * self.height
         for i in range(0, self.H_NUM_LINES):
-            lineY = i * ySpacing
+            lineY = i * ySpacing - self.currentYOffset
             x1, y1 = self.transform(xMin, lineY)
             x2, y2 = self.transform(xMax, lineY)
             self.horizontalLines[i].points = [x1, y1, x2, y2]
@@ -96,6 +102,15 @@ class MainWidget(Widget):
         xTransform = self.xPerspective + (xDiff * yFactor)
         yTransform = self.yPerspective - (yFactor * self.yPerspective)
         return int(xTransform), int(yTransform)
+
+    def update(self, dt): # dt for delta time
+        self.update_vertical_lines()
+        self.update_horizontal_lines()
+        self.currentYOffset += self.SPEED
+
+        ySpacing = self.H_LINE_SPACING * self.height
+        if self.currentYOffset >= ySpacing:
+            self.currentYOffset -= ySpacing
 
     def transform_2D(self, x, y):
         return int(x), int(y)
