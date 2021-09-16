eaed3b0187633dc294565801df17ba430e694fec
Josh Abbott <39091681+Aoshua@users.noreply.github.com>
2021-09-16 15:30:29 -0600

    added skeleton of prototype proj

--- prototype/main.py
+++ prototype/main.py
@@ -1,13 +1,14 @@
 #region Library Imports
 # The following 3 lines ensure a set inital window height
 from kivy.config import Config
-Config.set('graphics', 'width', '900')
-Config.set('graphics', 'height', '400')
+Config.set('graphics', 'width', '1728') # 1920 - 10%
+Config.set('graphics', 'height', '972') # 1080 - 10%
 
 from kivy.app import App
+from kivy.uix.relativelayout import RelativeLayout
 #endregion
 
-class MainWidget(RelativeLayout):
+class RootWidget(RelativeLayout):
     pass
 
 class PrototypeApp(App):
