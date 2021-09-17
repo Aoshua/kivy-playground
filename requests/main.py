427fc3dc475f03331408a82f5e75029fdffe4457
Josh Abbott <39091681+Aoshua@users.noreply.github.com>
2021-09-17 10:26:57 -0600

    demo requests > response to obj

--- requests/main.py
+++ requests/main.py
@@ -1,3 +1,4 @@
+from dictObj import DictObj
 from kivy.config import Config
 Config.set('graphics', 'width', '1728') # 1920 - 10%
 Config.set('graphics', 'height', '972') # 1080 - 10%
@@ -8,17 +9,39 @@ from kivy.properties import StringProperty
 import requests
 
 class RootWidget(RelativeLayout):
+    from dictObj import DictObj
     post = None
     post_title = StringProperty("Request not sent")
 
     def send_api_request(self):
         r = requests.get('https://api.dailysmarty.com/posts')
-        # r.json() returns a dictionary of the response
-        self.post = r.json()['posts'][0]
-        self.post_title = self.post['title']
+        postDict = r.json()['posts'][0]  # r.json() returns a dictionary of the response:
+        
+        # Convert to object with type:
+        self.post = Post(postDict)
+        self.post_title = self.post.title
+
+        # Simply access as dictionary:
+        # self.post = r.json()['posts'][0]
+        # self.post_title = self.post['title']
 
     def on_request_btn_pressed(self):
         self.send_api_request()
+
+class Post():
+    id: int
+    title: str
+    content: str
+    created_at: str
+    url_for_post: str
+    associated_topics: list
+    post_links: list
+
+    def __init__(self, dict:dict):
+        for key in dict:
+            setattr(self, key, dict[key])
+   
+
 
 class RequestsApp(App):
     pass
