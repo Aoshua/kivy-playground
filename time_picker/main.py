fecbdb7e78d7dc5eeda85dcd5564f0f76292f9f7
Josh Abbott <39091681+Aoshua@users.noreply.github.com>
2021-09-17 15:06:27 -0600

    testing kivymd apk using dp

--- time_picker/main.py
+++ time_picker/main.py
@@ -4,12 +4,16 @@ from kivymd.uix.picker import MDTimePicker
 
 class MainApp(MDApp):
     def build(self):
-        self.theme_cls.theme_style = "Light"
+        self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "BlueGray"
         return Builder.load_file('time.kv')
 
     def show_time_picker(self):
+        from datetime import datetime
+        
+        default_time = datetime.strptime("4:20:00", '%H:%M:%S').time()
         time_dialog = MDTimePicker()
+        time_dialog.set_time(default_time)
         time_dialog.bind(on_cancel=self.on_cancel, time=self.get_time)
         time_dialog.open()
 
