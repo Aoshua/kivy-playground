6f8c185269bd51dd85480171cbc6bb2989ddd0d3
Josh Abbott <39091681+Aoshua@users.noreply.github.com>
2021-09-21 10:35:16 -0600

    added pagin to kivy_md demo

--- kivy_md/main.py
+++ kivy_md/main.py
@@ -1,18 +1,27 @@
 from kivy.lang import Builder
 from kivymd.app import MDApp
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+class LoginScreen(Screen):
+    def sign_in(self):
+        print('this happened')
+        # here we would send the credentials to an API
+        # before switching to a new page
+        self.manager.transition.direction = 'left'
+        self.manager.current = 'dashboard'
+
+class DashboardScreen(Screen):
+    pass
 
 class MainApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "LightGreen"
-        return Builder.load_file('awesome.kv')
+        Builder.load_file('awesome.kv')
 
-    def logger(self):
-        self.root.ids.welcome_label.text = f'Hey, {self.root.ids.user.text}!'
-
-    def clear(self):
-        self.root.ids.welcome_label.text = 'WELCOME'
-        self.root.ids.user.text = ''
-        self.root.ids.password.text = ''
+        sm = ScreenManager()
+        sm.add_widget(LoginScreen(name='login'))
+        sm.add_widget(DashboardScreen(name='dashboard'))
+        return sm
 
 MainApp().run()
